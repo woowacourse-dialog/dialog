95b88f3001085a9d89492fbabbedb4a2c21cd961
DongchannN <ldc991104@gmail.com>
2025-06-22 16:09:30 +0900

    fix: 세션에 담는 정보 기존 oauthId에서 id로 교체

--- server/src/test/java/com/dialog/server/service/AuthServiceTest.java
+++ server/src/test/java/com/dialog/server/service/AuthServiceTest.java
@@ -159,11 +159,11 @@ class AuthServiceTest {
     @DisplayName("등록된 사용자의 인증이 성공적으로 이루어진다")
     void authenticateSuccessTest() {
         // when
-        Authentication authentication = authService.authenticate(user.getOauthId());
+        Authentication authentication = authService.authenticate(user.getId());
 
         // then
         assertThat(authentication.isAuthenticated()).isTrue();
-        assertThat(authentication.getPrincipal()).isEqualTo(user.getOauthId());
+        assertThat(authentication.getPrincipal()).isEqualTo(user.getId());
         assertThat(authentication.getCredentials()).isNull();
 
         assertThat(authentication.getAuthorities()).hasSize(1);
@@ -173,7 +173,7 @@ class AuthServiceTest {
     @DisplayName("존재하지 않는 OAuth ID로 인증 시 예외가 발생한다")
     void authenticateWithNonExistingUserTest() {
         // when, then
-        assertThatThrownBy(() -> authService.authenticate(newOAuthId))
+        assertThatThrownBy(() -> authService.authenticate(999L))
                 .isInstanceOf(DialogException.class);
     }
 
@@ -181,7 +181,7 @@ class AuthServiceTest {
     @DisplayName("등록되지 않은 사용자 인증 시 예외가 발생한다")
     void authenticateUnregisteredUserTest() {
         // when, then
-        assertThatThrownBy(() -> authService.authenticate(tempUser.getOauthId()))
+        assertThatThrownBy(() -> authService.authenticate(tempUser.getId()))
                 .isInstanceOf(DialogException.class);
     }
 }

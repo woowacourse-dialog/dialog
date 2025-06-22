71894a48889aa6bf33196ec0beb398020f235276
DongchannN <ldc991104@gmail.com>
2025-06-22 14:56:34 +0900

    refactor: 메서드 시그니처 변경

--- server/src/main/java/com/dialog/server/repository/MessagingTokenRepository.java
+++ server/src/main/java/com/dialog/server/repository/MessagingTokenRepository.java
@@ -2,12 +2,11 @@ package com.dialog.server.repository;
 
 import com.dialog.server.domain.MessagingToken;
 import com.dialog.server.domain.User;
-import java.util.Collection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessagingTokenRepository extends JpaRepository<MessagingToken, Long> {
     List<MessagingToken> findMessagingTokensByUser(User user);
 
-    List<MessagingToken> findByUserIdIn(Collection<Long> userIds);
+    List<MessagingToken> findByUserIdIn(List<Long> userIds);
 }

75ee8f0a3f87542fc7aee8fa222f6db2b4efddd6
LeeJaeHoon <dlwogns3413@naver.com>
2025-08-31 17:37:08 +0900

    feat: 좋아요 상태에 따라 좋아요 초기화 UI 구현

--- client/src/api/like.js
+++ client/src/api/like.js
@@ -10,3 +10,8 @@ export async function deleteLikeDiscussion(discussionsId) {
   const res = await api.delete(`/discussions/${discussionsId}/likes`);
   return res.data;
 }
+
+export async function getLikeStatus(discussionsId) {
+  const res = await api.get(`/discussions/${discussionsId}/likes/status`);
+  return res.data;
+}

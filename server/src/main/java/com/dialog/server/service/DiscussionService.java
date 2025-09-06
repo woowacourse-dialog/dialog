d6d3d2f9c67d0eda1deb48ed92f84e8115a1f1f5
LeeJaeHoon <dlwogns3413@naver.com>
2025-09-07 00:16:03 +0900

    refactor: 줄바꿈 똑바로 정리

--- server/src/main/java/com/dialog/server/service/DiscussionService.java
+++ server/src/main/java/com/dialog/server/service/DiscussionService.java
@@ -110,7 +110,11 @@ public class DiscussionService {
 
         if (cursor == null || cursor.isEmpty()) {
             discussions = discussionRepository.findWithFiltersPageable(categories, statuses,
-                    PageRequest.of(0, pageSize + 1));
+                    PageRequest.of(
+                            0,
+                            pageSize + 1
+                    )
+            );
 //            discussions = discussionRepository.findFirstPageDiscussionsByDate(PageRequest.of(0, pageSize + 1));
         } else {
             String[] cursorParts = cursor.split(CURSOR_PART_DELIMITER);
@@ -144,12 +148,20 @@ public class DiscussionService {
         validatePageSize(size);
         List<Discussion> discussions;
         switch (searchType) {
-            case TITLE_OR_CONTENT ->
-                    discussions = searchDiscussionByTitleOrContentWithFilters(query, categories, statuses, cursor,
-                            size);
-            case AUTHOR_NICKNAME ->
-                    discussions = searchDiscussionByAuthorNicknameWithFilters(query, categories, statuses, cursor,
-                            size);
+            case TITLE_OR_CONTENT -> discussions = searchDiscussionByTitleOrContentWithFilters(
+                    query,
+                    categories,
+                    statuses,
+                    cursor,
+                    size
+            );
+            case AUTHOR_NICKNAME -> discussions = searchDiscussionByAuthorNicknameWithFilters(
+                    query,
+                    categories,
+                    statuses,
+                    cursor,
+                    size
+            );
             default -> throw new DialogException(ErrorCode.INVALID_SEARCH_TYPE);
         }
         return buildDateCursorResponse(discussions, size);

a0428de0da5493e9f2b5351f8f8bf05811270e59
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-12-13 21:39:18 +0900

    fix: Notification type EnumType 추가

--- server/src/main/java/com/dialog/server/domain/Notification.java
+++ server/src/main/java/com/dialog/server/domain/Notification.java
@@ -2,12 +2,15 @@ package com.dialog.server.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.lang.reflect.Type;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -34,6 +37,7 @@ public class Notification extends BaseEntity{
     @JoinColumn(name = "receiver_id", nullable = false)
     private User receiver;
 
+    @Enumerated(EnumType.STRING)
     private NotificationType type;
 
     private boolean isRead;
